30550224a8590f9628b30ce119bccec770d15aad
hocaron <kkannu0407@gmail.com>
2023-02-06 14:27:27 +0900

    feat: 서블릿 등록

---
+++ README.md
@@ -0,0 +1,11 @@
+# 토비의 스프링 부트 - 이해와 원리
+
+### Request
+- Request Line : Method, Path, HTTP Version
+- Headers
+- Message Body
+
+### Response
+- Status Line : Http Version, Status Code, Status Text
+- Headers
+- Message Body
--- src/main/java/com/study/tobyspring/TobySpringApplication.java
+++ src/main/java/com/study/tobyspring/TobySpringApplication.java
@@ -1,13 +1,35 @@
 package com.study.tobyspring;
 
-import org.springframework.boot.SpringApplication;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 @SpringBootApplication
 public class TobySpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TobySpringApplication.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet(
+				"ping",
+				new HttpServlet() {
+					@Override
+					public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+						res.setStatus(200);
+						res.setHeader("Content-Type", "text/plain");
+						res.getWriter().println("ping");
+					}
+				}).addMapping("/ping");
+		});
+		webServer.start();
 	}
 
 }
