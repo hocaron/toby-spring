3888a89c9b5fa554068b2c45ca1ae0755c2a9960
hocaron <kkannu0407@gmail.com>
2023-02-06 13:21:22 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/study/tobyspring/TobySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.tobyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
