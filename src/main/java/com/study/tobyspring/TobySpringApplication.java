92c27b56c66231942c806b91388a303b408768b4
hocaron <kkannu0407@gmail.com>
2023-02-06 19:38:02 +0900

    feat: MySpringApplication 분리

--- src/main/java/com/study/tobyspring/TobySpringApplication.java
+++ src/main/java/com/study/tobyspring/TobySpringApplication.java
@@ -1,35 +1,28 @@
 package com.study.tobyspring;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import com.study.tobyspring.ping.MySpringApplication;
 
 @Configuration
 @ComponentScan
 public class TobySpringApplication {
-	public static void main(String[] args) {
-		// spring container 생성 -> bean 등록 -> bean 초기화
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
 
-				// servlet container 생성 -> dispatcherServlet 등록 -> dispatcherServlet 초기화
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet(
-						"dispatcherServlet",
-						new DispatcherServlet(this)
-					).addMapping("/*");
-				});
-				webServer.start();
-			}
-		};
-		applicationContext.register(TobySpringApplication.class);
-		applicationContext.refresh();
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
+	public static void main(String[] args) {
+		MySpringApplication.run(TobySpringApplication.class, args);
 	}
 }
---
+++ src/main/java/com/study/tobyspring/ping/MySpringApplication.java
@@ -0,0 +1,33 @@
+package com.study.tobyspring.ping;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+
+	public static void run(Class<?> applicationClass, String... args) {
+		// spring container 생성 -> bean 등록 -> bean 초기화
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				// servlet container 생성 -> dispatcherServlet 등록 -> dispatcherServlet 초기화
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet(
+						"dispatcherServlet",
+						dispatcherServlet
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
+		applicationContext.register(applicationClass);
+		applicationContext.refresh();
+	}
+}
