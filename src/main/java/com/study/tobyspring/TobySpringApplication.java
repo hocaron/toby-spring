8d578e5a547b6c821e43f28cb94e3ffc8aefcfe3
hocaron <kkannu0407@gmail.com>
2023-02-06 18:44:23 +0900

    feat: 자바코드 구성 정보 사용

--- src/main/java/com/study/tobyspring/TobySpringApplication.java
+++ src/main/java/com/study/tobyspring/TobySpringApplication.java
@@ -1,21 +1,32 @@
 package com.study.tobyspring;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import com.study.tobyspring.ping.PingController;
+import com.study.tobyspring.ping.PingService;
 import com.study.tobyspring.ping.SimplePingService;
 
-@SpringBootApplication
+@Configuration
 public class TobySpringApplication {
+	@Bean
+	public PingController pingController(PingService pingService) {
+		return new PingController(pingService);
+	}
+
+	@Bean
+	public PingService pingService() {
+		return new SimplePingService();
+	}
 
 	public static void main(String[] args) {
 		// spring container 생성 -> bean 등록 -> bean 초기화
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -31,8 +42,7 @@ public class TobySpringApplication {
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(PingController.class);
-		applicationContext.registerBean(SimplePingService.class);
+		applicationContext.register(TobySpringApplication.class);
 		applicationContext.refresh();
 	}
 }
